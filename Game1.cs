cb42ad0f1ab52433f58ec5f0984f6aed2bdb2750
JustInCase <justin.kobiolka@outlook.de>
2025-09-26 10:56:27 +0200

    Added Simple Window Resize Function

--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Screens;
-using MonoGame.Extended.Screens.Transitions;
 using MonoPokeSharp.Screens;
 
 namespace MonoPokeSharp
@@ -16,6 +15,8 @@ namespace MonoPokeSharp
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            ResizeWindow(1280, 720);
         }
 
         protected override void LoadContent()
@@ -23,6 +24,12 @@ namespace MonoPokeSharp
             screenManager = new ScreenManager();
             Components.Add(screenManager);
             screenManager.LoadScreen(new MenuScreen(this));
+        }
+
+        public void ResizeWindow(int w, int h)
+        {
+            graphics.PreferredBackBufferWidth = w;
+            graphics.PreferredBackBufferHeight = h;
         }
     }
 }

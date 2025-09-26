6a98345354874fb5e890537a2328e79fc84a35cb
JustInCase <justin.kobiolka@outlook.de>
2025-09-26 13:24:15 +0200

    Worked on PLayer

--- Source/Entities/Player.cs
+++ Source/Entities/Player.cs
@@ -1,11 +1,59 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Graphics;
 
 namespace MonoPokeSharp.Entities
 {
     public class Player
     {
-        
+        public Vector2 Position;
+        public float Speed;
+        public enum Direction
+        {
+            Down,
+            Left,
+            Right,
+            Up
+        }
+        public enum State
+        {
+            Idle,
+            Walking
+        }
+        public Texture2DAtlas PlayerTexture;
+        public SpriteSheet PlayerSpriteSheet;
+        public AnimatedSprite PlayerSprite;
+        public Player(float x, float y)
+        {
+            Position.X = x;
+            Position.Y = y;
+            Speed = 150;
+        }
+
+        public void LoadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+
+        }
+
+        public void HandleInput()
+        {
+
+        }
+
+        public void Move()
+        {
+            
+        }
     }
 }
